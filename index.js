4158196c403ef62ecee63c5bd3a083ac66bbfbc2
DIGITALMAHESH720 <124596441+DIGITALMAHESH720@users.noreply.github.com>
2023-09-22 18:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const http = require('http');
+// This function adds a new row to the table with the form data
+function addTableRow() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const dob = document.getElementById('dob').value;
+  const acceptedTerms = document.getElementById('acceptedTerms').checked;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+  const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+  const newRow = table.insertRow(table.rows.length);
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  const cell3 = newRow.insertCell(2);
+  const cell4 = newRow.insertCell(3);
+  const cell5 = newRow.insertCell(4);
 
-const PORT = process.env.PORT || 3000;
+  cell1.innerHTML = name;
+  cell2.innerHTML = email;
+  cell3.innerHTML = password;
+  cell4.innerHTML = dob;
+  cell5.innerHTML = acceptedTerms ? 'Yes' : 'No';
 
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  // Reset the form after submission
+  document.getElementById('registrationForm').reset();
+}
+
+// Event listener for form submission
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  addTableRow();
 });
